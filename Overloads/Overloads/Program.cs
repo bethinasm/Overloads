9997e590be7eb30ca7ba2d36cc2b6a1da09b5ab6
Bethina S.M <bethina.s.moen@gmail.com>
2024-12-09 13:36:53 +0100

    Oppgave tekst

---
+++ Overloads/Overloads/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Oppgave: Overloads");
+/*
+    Test ut overloads og default verdier selv ved å lage 
+    en metode PrintWelcomeMessage() som printer ut "Hei og velkommen".
+   
+    Lag en overload metode som kan ta inn et kompliment og legge det 
+    til i velkomstmeldingen, dette navnet skal default være "Du er snill!", 
+    med mindre noe annet er oppgitt.
+ */
